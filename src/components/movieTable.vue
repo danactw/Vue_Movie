d794ebb0c6ff43f9413c567b4bcd73d5200a2d70
danachen <danac.tw@gmail.com>
2022-03-10 13:41:45 +0800

    component - tableRow

--- src/components/movieTable.vue
+++ src/components/movieTable.vue
@@ -1,9 +1,11 @@
 <script>
+import TableRow from "../tableRow.vue";
   export default {
     props: {
-      slicedData: Array,
-    }
-  }
+        slicedData: Array,
+    },
+    components: { TableRow }
+}
 </script>
 
 <template>
@@ -19,14 +21,15 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="movie in slicedData" :key="movie">
+      <TableRow v-for="movie in slicedData" :key="movie" v-bind="movie" />
+      <!-- <tr v-for="movie in slicedData" :key="movie">
         <td> {{ movie.rank }} </td>
         <td><a href="連結">{{ movie.title }}</a></td>
         <td> {{ movie.year }} </td>
         <td> {{ movie.director }} </td>
         <td> {{ movie.main_stars }} </td>
         <td> {{ movie.rating }} </td>
-      </tr> 
+      </tr>  -->
     </tbody>
   </table>
   <div v-if="slicedData.length===0" >很抱歉！目前沒有您搜尋的電影！</div>
---
+++ src/tableRow.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  rank: String,
+  title: String,
+  year: String,
+  director: String,
+  main_stars: String,
+  rating: String
+})
+
+</script>
+
+<template>
+  <tr>
+    <td> {{ rank }} </td>
+    <td><a href="連結">{{ title }}</a></td>
+    <td> {{ year }} </td>
+    <td> {{ director }} </td>
+    <td> {{ main_stars }} </td>
+    <td> {{ rating }} </td>
+  </tr> 
+</template>
